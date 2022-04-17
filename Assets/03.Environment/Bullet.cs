6fd03f96912c3ad8570b3c9900bbbb6189af8fa3
BloodCryHiro <jo090989coding@gmail.com>
2022-04-18 00:45:15 +0800

    Setup basic bullet pool script

--- Assets/03.Environment/Bullet.cs
+++ Assets/03.Environment/Bullet.cs
@@ -5,10 +5,24 @@ public class Bullet : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float moveSpeed;
 
-    private void OnEnable()
+    private void Update()
     {
-        // TODO: Initialize bullet's fire direction
+        // Reset position
+        if (transform.position.y < -20f)
+        {
+            transform.position = transform.parent.position;
+            gameObject.SetActive(false);
+        }
+    }
 
-        rb.velocity = Vector2.down * moveSpeed;
+    private void FixedUpdate()
+    {
+        rb.velocity = transform.right * moveSpeed;
+    }
+
+    public void Initialize()
+    {
+        Vector3 direction = new Vector3(0f, 0f, Random.Range(-40f, 40f));
+        transform.Rotate(direction);
     }
 }
--- Assets/03.Environment/BulletPool.cs
+++ Assets/03.Environment/BulletPool.cs
@@ -11,9 +11,25 @@ public class BulletPool : MonoBehaviour
         bullets = new GameObject[bulletAmount];
         for (int i = 0; i < bulletAmount; i++)
         {
-            bullets[i] = Instantiate(bullet, transform.position, Quaternion.identity);
+            bullets[i] = Instantiate(bullet, transform.position, bullet.transform.rotation);
             bullets[i].SetActive(false);
             bullets[i].transform.parent = gameObject.transform;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+            ShootBullets();
+    }
+
+    private void ShootBullets()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            bullets[i].transform.position = new Vector2(Random.Range(-6f, 6f), 20);
+            bullets[i].SetActive(true);
+            bullets[i].GetComponent<Bullet>().Initialize();
         }
     }
 }
