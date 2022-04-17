b9992451eafce73f5512c6a70d10117bc26462b5
BloodCryHiro <jo090989coding@gmail.com>
2022-04-17 18:27:37 +0800

    Create Bullet script

---
+++ Assets/03.Environment/Bullet.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        // TODO: Initialize bullet's fire direction
+
+        Debug.Log("Bullet enabled");
+    }
+}
