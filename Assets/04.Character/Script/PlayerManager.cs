99a94d141f1c4e4fcd45b0844bc47e1a89d4a560
BloodCryHiro <jo090989coding@gmail.com>
2022-04-17 18:27:24 +0800

    Create PlayerManager script

---
+++ Assets/04.Character/Script/PlayerManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Bullet")
+        {
+            // TODO: Player death event
+
+            Debug.Log("Die");
+        }
+    }
+}
