2db3f9d7719367aa024ef8c253f7fdc57c2b0e8a
BloodCryHiro <jo090989coding@gmail.com>
2022-04-19 01:55:21 +0800

    Refactor shoot sector method to multiple waves

--- Assets/03.Environment/BulletPool.cs
+++ Assets/03.Environment/BulletPool.cs
@@ -8,10 +8,11 @@ public class BulletPool : MonoBehaviour
     [SerializeField] private GameObject bullet;
     private GameObject[][] rowPool;
     [SerializeField] private int rowWaves;
-    private int rowPerWaves = 11;
-    private List<GameObject> sectorPool = new List<GameObject>();
+    private int rowPerWave = 11;
+    // private List<GameObject> sectorPool = new List<GameObject>();
+    private GameObject[][] sectorPool;
     [SerializeField] private int sectorWaves;
-    private int sectorPoolCount;
+    private int sectorPerWave = 9;
 
     private void Start()
     {
@@ -19,36 +20,51 @@ public class BulletPool : MonoBehaviour
 
         for (int i = 0; i < rowPool.Length; i++)
         {
-            rowPool[i] = new GameObject[rowPerWaves];
+            rowPool[i] = new GameObject[rowPerWave];
         }
 
         for (int i = 0; i < rowWaves; i++)
         {
-            for (int j = 0; j < rowPerWaves; j++)
+            for (int j = 0; j < rowPerWave; j++)
             {
                 GameObject rowBullet = Instantiate(bullet, transform.position, Quaternion.identity);
                 rowPool[i][j] = rowBullet;
-                rowPool[i][j].SetActive(true);
+                rowPool[i][j].SetActive(false);
                 rowPool[i][j].transform.parent = transform;
             }
         }
 
-        sectorPoolCount = sectorWaves * 9;
-        for (int i = 0; i < sectorPoolCount; i++)
+        sectorPool = new GameObject[sectorWaves][];
+
+        for (int i = 0; i < sectorWaves; i++)
         {
-            GameObject sectorBullet = Instantiate(bullet, transform.position, Quaternion.identity);
-            sectorPool.Add(sectorBullet);
-            sectorBullet.SetActive(false);
-            sectorBullet.transform.parent = transform;
+            sectorPool[i] = new GameObject[sectorPerWave];
+        }
+
+        for (int i = 0; i < sectorWaves; i++)
+        {
+            for (int j = 0; j < sectorPerWave; j++)
+            {
+                GameObject sectorBullet = Instantiate(bullet, transform.position, Quaternion.identity);
+                sectorPool[i][j] = sectorBullet;
+                sectorPool[i][j].SetActive(false);
+                sectorPool[i][j].transform.parent = transform;
+            }
         }
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            StopCoroutine(ShootRowCoroutine());
             StartCoroutine(ShootRowCoroutine());
-        if (Input.GetKeyDown(KeyCode.W))
-            ShootSector();
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            StopCoroutine(ShootSectorCoroutine());
+            StartCoroutine(ShootSectorCoroutine());
+        }
     }
 
     private IEnumerator ShootRowCoroutine()
@@ -56,10 +72,10 @@ public class BulletPool : MonoBehaviour
         int gap = 2;
         for (int i = 0; i < rowWaves; i++)
         {
-            for (int j = 0; j < rowPerWaves; j++)
+            for (int j = 0; j < rowPerWave; j++)
             {
                 Vector2 bulletPosition;
-                bulletPosition.x = fireSource.transform.position.x - gap * (rowPerWaves / 2) + gap * j;
+                bulletPosition.x = fireSource.transform.position.x - gap * (rowPerWave / 2) + gap * j;
                 bulletPosition.y = fireSource.transform.position.y;
 
                 rowPool[i][j].transform.position = bulletPosition;
@@ -70,14 +86,19 @@ public class BulletPool : MonoBehaviour
         }
     }
 
-    private void ShootSector()
+    private IEnumerator ShootSectorCoroutine()
     {
         float angle = 20f;
-        for (int i = 0; i < sectorPoolCount; i++)
+        for (int i = 0; i < sectorWaves; i++)
         {
-            sectorPool[i].transform.Rotate(0f, 0f, -4 * angle + i * angle);
-            sectorPool[i].transform.position = fireSource.transform.position;
-            sectorPool[i].SetActive(true);
+            for (int j = 0; j < sectorPerWave; j++)
+            {
+                sectorPool[i][j].transform.Rotate(0f, 0f, -4 * angle + j * angle);
+                sectorPool[i][j].transform.position = fireSource.transform.position;
+                sectorPool[i][j].SetActive(true);
+            }
+
+            yield return new WaitForSeconds(0.2f);
         }
     }
 
