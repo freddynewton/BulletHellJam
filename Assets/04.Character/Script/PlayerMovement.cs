4740958bc811d72b6c7e44fb631cef4adc2b1229
BloodCryHiro <jo090989coding@gmail.com>
2022-04-22 09:44:54 +0800

    Create basic platform crossing

---
+++ Assets/04.Character/Script/PlayerMovement.cs
@@ -0,0 +1,92 @@
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rigidbody2D;
+
+    [SerializeField]
+    private float movementSpeed;
+
+    [SerializeField] private float dashTime;
+    [SerializeField] private float dashSpeed;
+
+    private Vector2 DirectionVector { get; set; }
+
+    private bool IsMoving { get; set; }
+
+    private Vector2 InputVector { get; set; }
+
+    private float CurrentDashTime { get; set; }
+    public UnityEvent onPlayerDashStart;
+    public UnityEvent onPlayerDashEnd;
+    private bool isDashEndInvoked = true;
+
+    private void Update()
+    {
+        ApplyMovement();
+    }
+
+    private void ApplyMovement()
+    {
+        GetDirectionVector();
+
+        Dash();
+
+        if (CurrentDashTime <= 0)
+        {
+            ApplyMovementVector();
+        }
+    }
+
+    private void ApplyMovementVector()
+    {
+        Vector2 currentPos = rigidbody2D.position;
+        Vector2 adjustedMovement = InputVector * movementSpeed;
+        rigidbody2D.MovePosition(currentPos + adjustedMovement * Time.fixedDeltaTime);
+    }
+
+    private void GetDirectionVector()
+    {
+        InputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        IsMoving = (InputVector != Vector2.zero) ? true : false;
+    }
+
+    private void Dash()
+    {
+        if (CurrentDashTime <= 0 && !isDashEndInvoked)
+        {
+            onPlayerDashEnd.Invoke();
+            isDashEndInvoked = true;
+        }
+
+        if (CurrentDashTime <= 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            // Invoke onPlayerDashStart event
+            onPlayerDashStart.Invoke();
+            isDashEndInvoked = false;
+
+            //TODO: Change CollisionLayer
+
+            CurrentDashTime = dashTime;
+            IsMoving = true;
+
+            // Player is dashing
+            var tempInputVec = InputVector;
+            rigidbody2D.velocity = tempInputVec * dashSpeed;
+        }
+        else if (CurrentDashTime >= 0)
+        {
+            //TODO: Change CollisionLayer
+
+            CurrentDashTime -= Time.deltaTime;
+        }
+    }
+
+}
