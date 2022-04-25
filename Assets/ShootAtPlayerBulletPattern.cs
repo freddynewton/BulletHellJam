8306cf89a896fc3aaa2c9c79641b8026e37619c4
Fred Newton <ghostlopment@gmail.com>
2022-04-25 21:01:33 +0200

    more pattern

--- Assets/ShootAtPlayerBulletPattern.cs
+++ Assets/ShootAtPlayerBulletPattern.cs
@@ -12,7 +12,7 @@ public class ShootAtPlayerBulletPattern : BulletPatternSpawner
             {
                 Bullet bullet = bulletSpawner.BasicBulletPool.Get();
                 bullet.transform.position = transform.position;
-                bullet.DirectionVector = transform.position - bulletSpawner.playerManager.transform.position;
+                bullet.DirectionVector = (bulletSpawner.playerManager.transform.position - transform.position).normalized;
                 yield return new WaitForSecondsRealtime(BulletSpawnTimeOffset);
 
             }
